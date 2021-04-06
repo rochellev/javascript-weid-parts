e0699338a28727d6ef77328b96ad0f0ae8934c0d
rochellev <rochellevroberts@utexas.edu>
2021-04-06 15:41:41 -0700

    add comments about using reference to outside envronment

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,12 +1,30 @@
+// execution context:
+// (1) phase 1 : allocate memory
+// (2) phase 2 : execute
+// each get a global object, 'this' and reference to outside environment
+
+function b() {
+  // will use reference to outside environment
+  // which is global context
+  console.log(myVar);
+  // prints 1
+}
+function a() {
+  var myVar = 2;
+  b();
+}
+var myVar = 1;
+a();
+
 // b();
 // var a = "hello";
-console.log(a);
+// console.log(a);
 
-if (a === undefined) {
-  console.log("a is undefined!");
-} else {
-  console.log("a is defined!");
-}
+// if (a === undefined) {
+//   console.log("a is undefined!");
+// } else {
+//   console.log("a is defined!");
+// }
 
 // function b() {
 //   console.log("called b! boi");
