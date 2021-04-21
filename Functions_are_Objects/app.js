d03925d8e28c4816ed25f0be346aa50a4490d82b
rochellev <rochellevroberts@utexas.edu>
2021-04-21 16:22:47 -0700

    establish that functions saved in memeory like objects

--- Functions_are_Objects/app.js
+++ Functions_are_Objects/app.js
@@ -1,4 +1,15 @@
 // functions are objects
-function greet(){
-  console.log('hi')
+// sits in memory
+function greet() {
+  console.log("hi");
 }
+
+// in memory
+greet.language = "english";
+
+console.log(greet);
+console.log(greet.language);
+
+// function object{ name: greet, code: console.log())}
+
+
