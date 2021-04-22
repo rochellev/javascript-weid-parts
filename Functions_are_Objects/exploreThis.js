82f1a88891a19fe9e944d8e77735a2f28763c130
rochellev <rochellevroberts@utexas.edu>
2021-04-22 11:15:48 -0700

    using this to access other properties withing object c

--- Functions_are_Objects/exploreThis.js
+++ Functions_are_Objects/exploreThis.js
@@ -16,3 +16,16 @@ b();
 
 // true evel though 3 execution contexts: global, a invoke and b invoke
 // this is still same location in memory
+
+// 'this' attached to obj that holds the function
+// access other properties and methods inside
+var c = {
+  name: "the c object",
+  log: function() {
+    this.name = "updated c obj";
+    console.log(this);
+  }
+};
+
+// invoke method attached to c
+c.log();
