9f792449f70bf73b459208d4df581d7cf0bf9a10
rochellev <rochellevroberts@utexas.edu>
2021-04-22 10:59:14 -0700

    understand more deeply by value and by reference

--- Functions_are_Objects/referencing.js
+++ Functions_are_Objects/referencing.js
@@ -4,7 +4,7 @@ var b;
 // copies in two different spots in memory
 b = a;
 a = 2;
-console.log(`a: ${a}   b: ${b}`);
+console.log(`a: ${a}\nb: ${b}`);
 
 // by reference -- all obj, including functions
 
@@ -14,6 +14,19 @@ var d;
 // pointing at same memory address
 d = c;
 c.greeting = "oh, hello";
-console.log(`c: ${c.greeting}   d: ${d.greeting}`);
-
 // see "oh hello" for both
+console.log(`c: ${c.greeting}\nd: ${d.greeting}`);
+
+// by reference -- even as parameters
+function changeGreeting(obj) {
+  obj.greeting = "Hola";
+}
+
+changeGreeting(d);
+console.log(`c: ${c.greeting}\nd: ${d.greeting}`);
+
+// equals operator sets up new memory space -- a new address for the new object
+// creates a spot for new obj and c points to that
+c = { greeting: "howdy" };
+// d and c no longer pointing at same memory location
+console.log(`c: ${c.greeting}\nd: ${d.greeting}`);
