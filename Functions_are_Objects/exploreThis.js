5358046c0290234a68add31e80c76b3f739a1987
rochellev <rochellevroberts@utexas.edu>
2021-04-22 11:11:42 -0700

    invoking points to global obj. see how add property to global obj

--- Functions_are_Objects/exploreThis.js
+++ Functions_are_Objects/exploreThis.js
@@ -1,5 +1,18 @@
 function a() {
   console.log(this);
+  // attach variable to global obj
+  this.newVariable = "hello";
 }
 
+var b = function() {
+  console.log(this);
+};
+
+// 'this' points to global object
+// function statement or expression
 a();
+console.log(newVariable);
+b();
+
+// true evel though 3 execution contexts: global, a invoke and b invoke
+// this is still same location in memory
