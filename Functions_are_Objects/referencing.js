2a2d449ce65c22d50c566089a00c530955e918e3
rochellev <rochellevroberts@utexas.edu>
2021-04-22 10:52:58 -0700

    objects are by reference, all see mutations

--- Functions_are_Objects/referencing.js
+++ Functions_are_Objects/referencing.js
@@ -6,5 +6,14 @@ b = a;
 a = 2;
 console.log(`a: ${a}   b: ${b}`);
 
+// by reference -- all obj, including functions
 
+var c = { greeting: "hi" };
+var d;
 
+// pointing at same memory address
+d = c;
+c.greeting = "oh, hello";
+console.log(`c: ${c.greeting}   d: ${d.greeting}`);
+
+// see "oh hello" for both
